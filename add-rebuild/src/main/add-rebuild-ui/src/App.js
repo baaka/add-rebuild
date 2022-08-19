9a0ec6cc818a85880550342230e3c3034b6902e9
Baaka <baaka@fina2.net>
2022-08-19 22:55:41 +0400

    add app form;

--- add-rebuild/src/main/add-rebuild-ui/src/App.js
+++ add-rebuild/src/main/add-rebuild-ui/src/App.js
@@ -1,26 +1,31 @@
 import './App.css';
 import MainContainer from "./api/container/MainContainer";
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import AppFormContainer from "./api/container/AppFormContainer";
+import Header from "./components/Header";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route
-                    path={"/app/appForm"}
-                    render={() => (
-                        <MainContainer/>
-                    )}
-                />
-                <Route
-                    path={"/app/donation"}
-                    render={() => (
-                        <MainContainer/>
-                    )}
-                />
-                <Redirect from="/" to="/app/appForm"/>
-            </Switch>
-        </BrowserRouter>
+        <div>
+            <Header/>
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        path={"/app/appForm"}
+                        render={() => (
+                            <AppFormContainer/>
+                        )}
+                    />
+                    <Route
+                        path={"/app/donation"}
+                        render={() => (
+                            <MainContainer/>
+                        )}
+                    />
+                    <Redirect from="/" to="/app/appForm"/>
+                </Switch>
+            </BrowserRouter>
+        </div>
     );
 }
 
