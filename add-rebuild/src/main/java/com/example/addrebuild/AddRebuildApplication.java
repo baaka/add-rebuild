143b530891717f41e2786a5636829ad68387cb73
Baaka <baaka@fina2.net>
2022-08-19 17:04:54 +0400

    initial commit;

---
+++ add-rebuild/src/main/java/com/example/addrebuild/AddRebuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.addrebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddRebuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddRebuildApplication.class, args);
+    }
+
+}
